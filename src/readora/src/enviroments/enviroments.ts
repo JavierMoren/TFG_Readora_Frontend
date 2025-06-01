7d87cf49a7ca4ef96657e2b2559d23965713fd7f
Javier Moreno Salas <moreno.salas.jav@gmail.com>
2025-06-01 12:12:38 +0200

    Agregado https

--- src/readora/src/enviroments/enviroments.ts
+++ src/readora/src/enviroments/enviroments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api',
-    apiBaseUrl: 'http://localhost:8080'
+    apiUrl: 'https://localhost:8443/api',
+    apiBaseUrl: 'https://localhost:8443'
   };
