16e25d5bfe823de304b9e205a099efb1294b6f01
Javier Moreno Salas <moreno.salas.jav@gmail.com>
2025-06-04 08:25:34 +0200

    Update ip para Despliegue

--- src/readora/src/enviroments/enviroments.ts
+++ src/readora/src/enviroments/enviroments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://3.89.110.211/api',
-    apiBaseUrl: 'https://3.89.110.211'
+    apiUrl: 'https://3.80.138.60/api',
+    apiBaseUrl: 'https://3.80.138.60'
   };
